f633580af2ffa8b0f4ad18bd802a3a57f7837931
Dasun lakshitha <42791160+Dasunlakshitha@users.noreply.github.com>
2021-10-01 13:26:35 +0530

    Init

---
+++ lib/button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttonTapped;
+
+  MyButton({this.color, this.textColor, this.buttonText, this.buttonTapped});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttonTapped,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
